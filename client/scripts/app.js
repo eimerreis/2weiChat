2e2e7323517b80ecd92ebf09b13ff637ac2e2dc1
eimerreis <imorice95@googlemail.com>
2015-12-01 16:49:14 +0000

    Angular Scope angepasst, damit die Socket.IO Nachricht gesendet wird

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,11 @@
     app.controller("ChatController", function($scope){
         
         $scope.sendMessage = function(){
-          socket.emit('chat-message', {"name": $scope.name, "message": $scope.message});  
+            if($scope.name != "" && $scope.message != "")
+            {
+                socket.emit('chat-message', {"name": $scope.name, "message": $scope.message});
+                console.log("Nachricht gesendet ------ " + $scope.name + ": " + $scope.message);
+            }
         };
     })
 })();
