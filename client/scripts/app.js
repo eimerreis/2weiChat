b5da3cca8c3ff0154ec3cbc3d2b30dd3701bcc9c
eimerreis <imorice95@googlemail.com>
2016-08-31 16:34:56 +0000

    Enter Message Change

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,9 +9,24 @@
         $scope.sendMessage = function(){
             if($scope.name != "" && $scope.message != "")
             {
+                $("#message-form").velocity("fadeOut", { delay: 100, duration: 500 });
+                $('#success-message').delay(600).velocity({opacity: 1}, { duration: 500, 
+                    complete: function(elements){
+                        $("#message-form input").each(function(){
+                           $(this).val(''); 
+                        });
+                    }
+                });
+                
+                //$("#new-message").velocity("fadeIn", {delay: 200, duration: 500});
                 socket.emit('chat-message', {"name": $scope.name, "message": $scope.message});
                 console.log("Nachricht gesendet ------ " + $scope.name + ": " + $scope.message);
             }
+        };
+        
+        $scope.closeMessage = function(){
+            $("#success-message").velocity({ opacity: 0}, 500);
+            $("#message-form").velocity("fadeIn", {delay: 500, duration: 500});
         };
     })
 })();
--- server.js
+++ server.js
@@ -5,6 +5,4 @@ var server = require('http').createServer(app);
 app.use(express.static(__dirname + "/client"));
 
 
-
-
 server.listen(process.env.PORT || 3000);
