c2746b8171d6a1e951ff630511b6aadcfe07e56b
Moritz Frölich <imorice95@googlemail.com>
2016-10-15 14:52:38 +0200

    implemented cookie setting when sending message for the first time

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,27 +3,33 @@
 /* global socket */
 (function(){
     var app = angular.module("chat", []);
-    
+
     app.controller("ChatController", function($scope){
-        
+
         $scope.sendMessage = function(){
             if($scope.name != "" && $scope.message != "")
             {
                 $("#message-form").velocity("fadeOut", { delay: 100, duration: 500 });
-                $('#success-message').delay(600).velocity({opacity: 1}, { duration: 500, 
+                $('#success-message').delay(600).velocity({opacity: 1}, { duration: 500,
                     complete: function(elements){
                         $("#message-form input").each(function(){
-                           $(this).val(''); 
+                          $(this).val('');
+                          $scope.message = '';
+                          $scope.name = ''
                         });
                     }
                 });
-                
+                var already = false;
+                if(document.cookie){
+                  already = true;
+                }
+                document.cookie = "alreadySentMessage=true";
                 //$("#new-message").velocity("fadeIn", {delay: 200, duration: 500});
-                socket.emit('chat-message', {"name": $scope.name, "message": $scope.message});
+                socket.emit('chat-message', {"name": $scope.name, "message": $scope.message, "alreadySent": already});
                 console.log("Nachricht gesendet ------ " + $scope.name + ": " + $scope.message);
             }
         };
-        
+
         $scope.closeMessage = function(){
             $("#success-message").velocity({ opacity: 0}, 500);
             $("#message-form").velocity("fadeIn", {delay: 500, duration: 500});
